050355ce68814cd1fbb1fdfd03f1a1782047d918
holyhgrenade <>
2023-09-13 21:07:17 +0200

    Input checks

--- speedtest_logger.py
+++ speedtest_logger.py
@@ -4,10 +4,19 @@ import argparse
 import sys
 
 def measure_speed(logfile, timediff, measurements):
+    if timediff <= 0:
+        print("Error: 'timediff' must be a positive integer.")
+        return
+
+    if measurements <= 0:
+        print("Error: 'measurements' must be a positive integer.")
+        return
+
     with open(logfile, 'w') as log_file:
         log_file.write("timestamp\tdownload_speed\tupload_speed\tping\n")
 
         for _ in range(measurements):
+            start_time = time.time()
             st = speedtest.Speedtest(secure = True)
             st.get_best_server()
 
@@ -21,8 +30,9 @@ def measure_speed(logfile, timediff, measurements):
             log_file.write(log_line)
 
             print(f"Measurement at {timestamp}: Download: {download_speed:.2f} Mbps, Upload: {upload_speed:.2f} Mbps, Ping: {ping} ms")
-
-            time.sleep(timediff)
+            end_time = time.time()
+            sleep_time = timediff - ( (end_time - start_time) % timediff )
+            time.sleep(sleep_time)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Measure and log internet speed.")
